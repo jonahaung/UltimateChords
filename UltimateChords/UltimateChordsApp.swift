d838b1b255539db4c28037f52ad191ee5f78f4bd
Aung Ko Min <jonahaung@gmail.com>
2022-03-15 23:27:09 +0800

    Initial Commit

---
+++ UltimateChords/UltimateChordsApp.swift
@@ -0,0 +1,20 @@
+//
+//  UltimateChordsApp.swift
+//  UltimateChords
+//
+//  Created by Aung Ko Min on 15/3/22.
+//
+
+import SwiftUI
+
+@main
+struct UltimateChordsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
